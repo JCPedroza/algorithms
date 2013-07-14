36a66bf6c3c940377b174a9ac561f94b222f1cf9
JCPedroza <nemesis4go10@hotmail.com>
2013-07-14 01:46:54 -0600

    attempt to add an accurate action counter to bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -14,4 +14,31 @@ def bubble_sort(alist):
                 return_list[e], return_list[e + 1] = return_list[e + 1], return_list[e]
     return return_list
 
-print bubble_sort([7, 3, 4, 8, 2, 1, 9, 6, 5, 10])
+
+def bubble_sort_counter(alist):
+    return_list = alist
+    length = len(return_list) - 1
+    is_sorted = False
+    counter = 0
+    # Will continue iterating until no swap is made, in that case
+    # is_sorted = True and the sorted list is returned.
+    while not is_sorted:
+        is_sorted = True
+        for e in range(length):
+            # Counts iteration
+            counter += 1
+            if return_list[e] > return_list[e + 1]:
+                # Counts comparison
+                counter += 1
+                # flag that there was a swap in the scan
+                is_sorted = False
+                # swap items
+                return_list[e], return_list[e + 1] = return_list[e + 1], return_list[e]
+                # Counts swap (should I be adding 2 instead of 1?)
+                counter += 1
+    return return_list, counter
+
+print bubble_sort_counter(([4,3,2,1]))
+print bubble_sort_counter([5,4,3,2,1])
+print bubble_sort_counter([6,5,4,3,2,1])
+print bubble_sort_counter([7,6,5,4,3,2,1])
