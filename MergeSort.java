f2c3cf9b86e2b54bd0f567d85163fd1b8b989a5d
JCPedroza <nemesis4go10@hotmail.com>
2013-07-17 18:31:25 -0600

    expanded MergeSort.java and BubbleSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class MergeSort{
 
     public static void main(String[] args){
-
-
+        System.out.println("MergeSort algorithm");
     }
 }
