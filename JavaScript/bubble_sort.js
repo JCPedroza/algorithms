6cf3ab733550cde5de06128bc4b9a1262484c9c7
JCPedroza <nemesis4go10@hotmail.com>
2013-08-24 14:48:27 -0600

    implemented tests, random array builder, and some comments and formatting

--- JavaScript/bubble_sort.js
+++ JavaScript/bubble_sort.js
@@ -1,3 +1,7 @@
+// ========================================
+//             Algorithm Area
+// ========================================
+
 // Returns the sorted version of the input array
 function bubbleSort(theArray){
     var returnArray = theArray;
@@ -9,11 +13,40 @@ function bubbleSort(theArray){
         isSorted = true;
         for (var i = 0; i < length; i++){
             if (returnArray[i] > returnArray[i + 1]){
-                isSorted = false; // flag that there was a swap in the scan
-                returnArray[i]     = returnArray[i + 1]; // swap items
-                returnArray[i + 1] = returnArray[i];     // swap items
+                isSorted           = false;               // flag that there was a swap in the scan
+                var toSwap         = returnArray[i];      // buffer to use in swap
+                returnArray[i]     = returnArray[i + 1];  // swap items
+                returnArray[i + 1] = toSwap;              // swap items
             }
         }
     }
     return returnArray;
 }
+
+// ========================================
+//                Test Area
+// ========================================
+
+// Produces an array with random integers between min and max
+function makeRandomArray(size, max, min){
+    var returnArray = [];
+    for (var i = 0; i < size; i++){
+        returnArray[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    return returnArray;
+}
+
+// Array declarations to use in tests
+var testArray1 = [3, 2, 1];
+var testArray2 = [10, 7, 6, 99, 120, 20, 59, 48, 13];
+var testArray3 = makeRandomArray(10, 0, 10);
+
+// Tests
+var testArray1Sorted = bubbleSort(testArray1);
+var testArray2Sorted = bubbleSort(testArray2);
+var testArray3Sorted = bubbleSort(testArray3);
+
+// Prints
+console.log(testArray1Sorted);
+console.log(testArray2Sorted);
+console.log(testArray3Sorted);
