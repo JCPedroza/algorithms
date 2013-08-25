f2421c86b681d6f3b73dc0e22d9e187dfa79daf4
JCPedroza <nemesis4go10@hotmail.com>
2013-08-24 18:46:23 -0600

    fixed bug with constructor and sz. Sz should be initialized to 1, and it does now

--- Algorithms1-Princeton/QuickUnionUFImproved.java
+++ Algorithms1-Princeton/QuickUnionUFImproved.java
@@ -45,7 +45,7 @@ public class QuickUnionUFImproved{
         sz    = new int[N];
         for (int i = 0; i < N; i++){
             id[i] = i;
-            sz[i] = i;
+            sz[i] = 1;
         }
     }
     
