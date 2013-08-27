3e2c5cf8074b5378bc4308c1a2eeb8d42cadd632
JCPedroza <nemesis4go10@hotmail.com>
2013-08-27 17:44:18 -0600

    implemented and tested makeRandomArray() and makeManyRandomArrays(). next: test the run times

--- Algorithms1-Princeton/3-Sum/ThreeSumTests.java
+++ Algorithms1-Princeton/3-Sum/ThreeSumTests.java
@@ -1,7 +1,8 @@
-import java.lang.reflect.Method; // used for reflection, to use methods as parameters
+import java.util.Arrays;         // To produce a string from an int array.
+import java.lang.reflect.Method; // Used for reflection, to use methods as parameters.
 
 /**
-* Class with the main method, used to perform tests on the 3-sum algorithms
+* Class with the main method, used to perform tests on the 3-sum algorithms.
 */
 public class ThreeSumTests{
     
@@ -50,16 +51,32 @@ public class ThreeSumTests{
     }
 
     /**
-    * Generates an array of random numbers
+    * Generates an array of random numbers.
     * @param size Size of the array to be generated.
-    * @param min  Minimum value.
-    * @param max  Maximum value.
-    * @return An array of random numbers
+    * @param min  Minimum value (inclusive).
+    * @param max  Maximum value (inclusive).
+    * @return An array of random numbers.
     */
     private static int[] makeRandomArray(int size, int min, int max){
         int[] returnArray = new int[size];
         for (int i = 0; i < size; i++) {
             returnArray[i] = (int) (Math.random() * (max - min + 1)) + min;
+        }
+        return returnArray;
+    }
+
+    /**
+    * Generates an array of arrays of random numbers.
+    * @param howMany Number of arrays to be generated.
+    * @param size    Size of the arrays to be generated.
+    * @param min     Minimum value (inclusive).
+    * @param max     Maximum value (inclusive).
+    * @return An array of arrays of random numbers.
+    */
+    private static int[][] makeManyRandomArrays(int howMany, int size, int min, int max){
+        int[][] returnArray = new int[howMany][];
+        for (int i = 0; i < howMany; i++){
+            returnArray[i] = makeRandomArray(size, min, max);
         }
         return returnArray;
     }
@@ -78,12 +95,19 @@ public class ThreeSumTests{
         ThreeSumBruteForce   TSBF = new ThreeSumBruteForce();
         ThreeSumBinarySearch TSBS = new ThreeSumBinarySearch();
 
+
         // ==========
         // Arrays
         // ==========
 
         int[] array1 = new int[] {1, -2, 3, -3, -1, 4, -4, 5, -5}; 
         int[] array2 = new int[] {3, 4, 5, 6};  
+
+        // testing makeRandomArray()
+        int[] array0 = makeRandomArray(10, -10, 10);
+
+        // testing makeManyRandomArrays()
+        int[][] arrayOfArrays1 = makeManyRandomArrays(10, 10, -10, 10);
 
         // ==========
         // Prints
@@ -96,5 +120,14 @@ public class ThreeSumTests{
         // Binary search approach, checking everything is ok:
         // System.out.println(TSBS.count(array1)); // 6
         // System.out.println(TSBS.count(array2)); // 0
+
+        // Testing makeRandomArray()
+        // System.out.println(Arrays.toString(array0));
+
+        // Testing makeManyRandomArrays()
+        for (int[] anArray : arrayOfArrays1){
+            System.out.println(Arrays.toString(anArray));
+        }
+        
     }
 }
