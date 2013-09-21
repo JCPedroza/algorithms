a2c63abba4848c92209453bc7988f0ba64f695af
JCPedroza <nemesis4go10@hotmail.com>
2013-09-21 01:13:08 -0600

    improved comments

--- JavaScript/sorting/insertion.js
+++ JavaScript/sorting/insertion.js
@@ -44,13 +44,13 @@
 * @param a The array to be sorted.
 */
 var sort = function(a){
-    N = a.length;                       // length of the array
-    for (i = 0; i < N; i++){            // loop through the array
-        for (j = i; j > 0; j--){        // loop from index i to 1
-            if (a[j] < a[j - 1]){       // if index j < index j - 1:
-                exch(a, j, j - 1);      // swap those items in the array
+    N = a.length;                  // length of the array
+    for (i = 0; i < N; i++){       // loop through the array
+        for (j = i; j > 0; j--){   // loop from index i to 1
+            if (a[j] < a[j - 1]){  // if index j < index j - 1:
+                exch(a, j, j - 1); // swap those items in the array
             }
-            else break;                 // break the loop if j > j -1
+            else break;            // break the loop if j > j -1
         }
     }
 };
