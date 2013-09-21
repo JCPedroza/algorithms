d8475e4f130d486e109aba7f39ba65bce6c15a73
JCPedroza <nemesis4go10@hotmail.com>
2013-09-21 00:57:30 -0600

    more tests

--- Algorithms1-Princeton/Union-Find/UnionFindTests.java
+++ Algorithms1-Princeton/Union-Find/UnionFindTests.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class QuickUnionTests{
+public class UnionFindTests{
     
     public static void main(String[] args){
         
@@ -8,6 +8,7 @@ public class QuickUnionTests{
         int                  n          = 10; // Number of isolated items
         QuickFindUF          QF10       = new QuickFindUF(n);
         QuickUnionUF         QUUF10     = new QuickUnionUF(n);
+        QuickUnionWeightedUF QUWUF10    = new QuickUnionWeightedUF(n);
         QuickUnionUFImproved QUUFI10    = new QuickUnionUFImproved(n);
         // To use as a string in the prints that represent objects in the array
         int[]                objects    = new int[n];
@@ -15,68 +16,29 @@ public class QuickUnionTests{
             objects[i] = i;
         }
         String               objectsStr = Arrays.toString(objects);
-
-        // Check arrays
-        System.out.println("");
-        System.out.println("============================================");
-        System.out.println("Initial state of arrays: ");
-        System.out.println("");
-        System.out.println("Quick Find:");
-        System.out.println("Object: " + objectsStr);
-        System.out.println("ID:     " + Arrays.toString(QF10.getId()));
-        System.out.println("");
-        System.out.println("Quick Union:");
-        System.out.println("Object: " + objectsStr);
-        System.out.println("ID:     " + Arrays.toString(QUUF10.getId()));
-        System.out.println("");
-        System.out.println("Quick Union Improved:");
-        System.out.println("Object: " + objectsStr);
-        System.out.println("ID:     " + Arrays.toString(QUUFI10.getId()));
-        System.out.println("T Size: " + Arrays.toString(QUUFI10.getSz()));
         
-        // Perform union(1, 2);
-        QF10.union(1, 2);
-        QUUF10.union(1, 2);
-        QUUFI10.union(1, 2);
-        // Print after union(1, 2):
-        System.out.println("");
-        System.out.println("============================================");
-        System.out.println("After union(1, 2): ");
-        System.out.println("");
-        System.out.println("Quick Find:");
-        System.out.println("Object: " + objectsStr);
-        System.out.println("ID:     " + Arrays.toString(QF10.getId()));
-        System.out.println("");
-        System.out.println("Quick Union:");
-        System.out.println("Object: " + objectsStr);
-        System.out.println("ID:     " + Arrays.toString(QUUF10.getId()));
-        System.out.println("");
-        System.out.println("Quick Union Improved:");
-        System.out.println("Object: " + objectsStr);
-        System.out.println("ID:     " + Arrays.toString(QUUFI10.getId()));
-        System.out.println("T Size: " + Arrays.toString(QUUFI10.getSz()));
+        // Actions
+        QF10.union(7, 5);
+        QF10.union(8, 7);
+        QF10.union(3, 7);
+        QF10.union(9, 2);
+        QF10.union(8, 0);
+        QF10.union(4, 3);
 
-        // Perform union(9, 1);
-        QF10.union(9, 1);
-        QUUF10.union(9, 1);
-        QUUFI10.union(9, 1);
-        // Print after union(1, 2):
-        System.out.println("");
-        System.out.println("============================================");
-        System.out.println("After union(9, 1): ");
-        System.out.println("");
-        System.out.println("Quick Find:");
-        System.out.println("Object: " + objectsStr);
-        System.out.println("ID:     " + Arrays.toString(QF10.getId()));
-        System.out.println("");
-        System.out.println("Quick Union:");
-        System.out.println("Object: " + objectsStr);
-        System.out.println("ID:     " + Arrays.toString(QUUF10.getId()));
-        System.out.println("");
-        System.out.println("Quick Union Improved:");
-        System.out.println("Object: " + objectsStr);
-        System.out.println("ID:     " + Arrays.toString(QUUFI10.getId()));
-        System.out.println("T Size: " + Arrays.toString(QUUFI10.getSz()));
+        QUWUF10.union(6, 9);
+        QUWUF10.union(2, 4);
+        QUWUF10.union(8, 0);
+        QUWUF10.union(5, 9);
+        QUWUF10.union(0, 9);
+        QUWUF10.union(3, 1);
+        QUWUF10.union(2, 3);
+        QUWUF10.union(5, 2);
+        QUWUF10.union(9, 7);
+
+        // Prints
+        System.out.println(Arrays.toString(QF10.getId()));
+        System.out.println(Arrays.toString(QUWUF10.getId()));
+        
 
 
     }
