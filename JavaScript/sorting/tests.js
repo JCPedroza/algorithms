c5ca469dcbb3df76e75f21b9545eaf8abc416763
JCPedroza <nemesis4go10@hotmail.com>
2013-09-21 01:03:02 -0600

    added tests for insertion

---
+++ JavaScript/sorting/tests.js
@@ -0,0 +1,16 @@
+// =================================================
+//                 Import Modules
+// =================================================
+
+var insertion = require("./insertion");
+
+
+// =================================================
+//             Collection Declarations
+// =================================================
+
+var simpleArray1 = [7, 5, 3, 2, 1, 8, 9, 6, 4];
+
+insertion.sort(simpleArray1);
+
+console.log(simpleArray1); // it works
