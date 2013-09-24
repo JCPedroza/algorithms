be2599ba29856c1164448b9150f543ebe39ff1c8
JCPedroza <nemesis4go10@hotmail.com>
2013-09-24 03:55:20 -0600

    first implementation and comments

---
+++ Scala/EuclidsGCD.scala
@@ -0,0 +1,7 @@
+object EuclidsGCD{
+
+  /** Computes the greatest common divisor of two numbers using Euclid's algorithm */
+  def gcd(a: Int, b: Int): Int = 
+    if (b == 0) a else gcd(b, a % b)
+
+}
