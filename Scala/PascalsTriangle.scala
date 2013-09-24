9705ad53ab485763cccf790b722e0628994b2157
JCPedroza <nemesis4go10@hotmail.com>
2013-09-24 09:00:50 -0600

    implemented printPT()

--- Scala/PascalsTriangle.scala
+++ Scala/PascalsTriangle.scala
@@ -40,4 +40,17 @@ object PascalsTriangle{
   def pascal(c: Int, r: Int): Int = 
     if (c == 0 || c == r) 1                      // base case: edges are always 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1) // apply pascal's rule recursively
+  
+  /**
+  * Prints a pascal triangle of r rows.
+  */
+  def printPT(r: Int) {
+    println("Pascal's Triangle")
+    for (row <- 0 to r) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+  
 }
