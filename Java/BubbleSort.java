9ad14fe4f5646f23c819f501d8f55eead589a1f0
JCPedroza <nemesis4go10@hotmail.com>
2013-09-29 14:45:36 -0600

    improved comments

--- Java/BubbleSort.java
+++ Java/BubbleSort.java
@@ -11,19 +11,19 @@ public class BubbleSort{
 
     public static void descendingBubbleSort( int [ ] num ){
         int j;
-        boolean flag = true;   // set flag to true to begin first pass
-        int temp;   //holding variable
+        boolean flag = true;                 // set flag to true to begin first pass
+        int temp;                            //holding variable
         while ( flag ){
-            flag= false;    //set flag to false awaiting a possible swap
+            flag= false;                     //set flag to false awaiting a possible swap
             for( j=0;  j < num.length -1;  j++ ){
                 if ( num[ j ] < num[j+1] ){  // change to > for ascending sort       
-                    temp = num[ j ];                //swap elements
+                    temp = num[ j ];         //swap elements
                     num[ j ] = num[ j+1 ];
                     num[ j+1 ] = temp;
-                    flag = true;              //shows a swap occurred  
+                    flag = true;             //shows a swap occurred  
                 } 
             } 
         } 
     } 
     
-} // End class
+} 
