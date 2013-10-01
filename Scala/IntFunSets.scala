3dfdd1c398561d6adf4b87c9c22fc63d54ef845e
JCPedroza <nemesis4go10@hotmail.com>
2013-10-01 15:50:28 -0600

    tested, improved some comments and javadocs

--- Scala/IntFunSets.scala
+++ Scala/IntFunSets.scala
@@ -1,3 +1,26 @@
+/**
+* Purely functional sets of integers. 
+*
+* Representation:
+* 
+* As an example to motivate our representation, how would you represent the set of all negative 
+* integers? You cannot list them all… one way would be so say: if you give me an integer, I can 
+* tell you whether it’s in the set or not: for 3, I say no; for -1, I say yes.
+*
+* Mathematically, we call the function which takes an integer as argument and which returns a 
+* boolean indicating whether the given integer belongs to a set, the characteristic function of 
+* the set. For example, we can characterize the set of negative integers by the characteristic 
+* function (x: Int) => x < 0.
+*
+* Therefore, we choose to represent a set by its characterisitc function and define a type alias 
+* for this representation:
+* type Set = Int => Boolean
+* Using this representation, we define a function that tests for the presence of a value in a set:
+* def contains(s: Set, elem: Int): Boolean = s(elem)
+*
+* In order to limit the search space, we set the range of forall and exists to a bound value; the 
+* scan will be made from -bound to bound (from -1000 to 1000 if bound = 1000).
+*/
 object IntFunSets {
   /**
   * We represent a set by its characteristic function, i.e.
@@ -44,7 +67,8 @@ object IntFunSets {
     intersect(s, p)
 
   /**
-  * The bounds for `forall` and `exists` are +/- 1000.
+  * The bounds for `forall` and `exists` are +/- 1000, 
+  * in order to limit the search space.
   */
   val bound = 1000
 
