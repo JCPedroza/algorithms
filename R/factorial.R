7995571aa81f03aae9f7115440bdd1494593ea8c
JCPedroza <nemesis4go10@hotmail.com>
2013-10-05 02:32:36 -0600

    finished timing tests

--- R/factorial.R
+++ R/factorial.R
@@ -1,3 +1,6 @@
+# Different implementations of a simple factorial algorithm.
+# To run this script: source("factorial.R")
+
 # ===============================
 #        The Algorithms
 # ===============================
@@ -19,17 +22,38 @@ factorialTailRecursive <- function(n){
 
 # Computes factorial of n using iteration and while loop.
 factorialIterW <- function(n){
-  acc = 1
+  acc <- 1
   while (n > 1){
-    acc = acc * n
-    n   = n - 1
+    acc <- acc * n
+    n   <- n - 1
   }
   acc
 }
 
 # Computes factorial of n using iteration and for loop.
 factorialIterF <- function(n){
-  acc = 1
+  acc <- 1
   for (i in n:1) acc = acc * i
   acc
 }
+
+# ===============================
+#      Timing the Algorithms
+# ===============================
+
+print("")
+print("factorialRecursive()")
+print(system.time(replicate(2000, factorialRecursive(150))))
+
+print("")
+print("factorialTailRecursive()")
+print(system.time(replicate(2000, factorialTailRecursive(150))))
+
+print("")
+print("factorialIterW()")
+print(system.time(replicate(2000, factorialIterW(150))))
+
+print("")
+print("factorialIterF()")
+print(system.time(replicate(2000, factorialIterF(150))))
+
