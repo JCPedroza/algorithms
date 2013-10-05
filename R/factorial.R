01ac4d50aafd51dc9a57fbb00a5dfdc6e36a6cff
JCPedroza <nemesis4go10@hotmail.com>
2013-10-05 02:03:32 -0600

    added 3 more algorithms, still needs timing tests

--- R/factorial.R
+++ R/factorial.R
@@ -1,4 +1,35 @@
-factorial <- function(n){
+# ===============================
+#        The Algorithms
+# ===============================
+
+# Computes factorial of n using recursion.
+factorialRecursive <- function(n){
   if (n == 1) 1
   else n * (factorial(n - 1))
+}
+
+# Computes factorial of n using tail recursion.
+factorialTailRecursive <- function(n){
+  recursion <- function(n, acc){
+    if (n < 1) acc
+    else recursion(n-1, acc * n)
+  }
+  recursion(n, 1)
+}
+
+# Computes factorial of n using iteration and while loop.
+factorialIterW <- function(n){
+  acc = 1
+  while (n > 1){
+    acc = acc * n
+    n   = n - 1
+  }
+  acc
+}
+
+# Computes factorial of n using iteration and for loop.
+factorialIterF <- function(n){
+  acc = 1
+  for (i in n:1) acc = acc * i
+  acc
 }
