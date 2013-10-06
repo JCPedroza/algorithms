99ac451337d1226936ee1efb15afef16f767490a
JCPedroza <nemesis4go10@hotmail.com>
2013-10-05 22:08:13 -0600

    implemented new performance dynamic using the resource module, now can calculate both cpu time and wall clock time

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,5 +1,6 @@
 import timeit
 import time
+import resource
 
 """
 Different implementations of a simple factorial algorithm.
@@ -51,37 +52,60 @@ def performance(f, num):
     for i in range(0, repeats): f(num)
     return time.clock() - start
 
+def resource_performance(f, num):
+    """
+    Measures running time of a function using the resource module.
+    Index 0 is user time, index 1 is system time.
+    """
+    start = resource.getrusage(resource.RUSAGE_SELF)
+    for i in range(0, repeats): f(num)
+    end   = resource.getrusage(resource.RUSAGE_SELF)
+    return (end.ru_utime - start.ru_utime, 
+            end.ru_stime - start.ru_stime)
+
 print ""
 print "==========================================================================="
 print ""
 print "Running time of different factorial algorithm implementations."
-print "Uses timeit module and a custom performance function."
+print "Uses timeit module, resource module, and a custom performance function."
 print ""
 print "Compute {0} factorial {1} times:".format(num, repeats)
 
 timer1 = timeit.Timer("factorial_recursive(150)", "from __main__ import factorial_recursive")
 print ""
 print "factorial_recursive()"
-print "timeit: " + str(timer1.timeit(repeats))
-print "custom: " + str(performance(factorial_recursive, num))
+print "timeit:          " + str(timer1.timeit(repeats))
+print "custom:          " + str(performance(factorial_recursive, num))
+factorial_recursive_resource = resource_performance(factorial_recursive, num)
+print "resource user:   " + str(factorial_recursive_resource[0])
+print "resource system: " + str(factorial_recursive_resource[1])
 
 timer2 = timeit.Timer("factorial_tail_recursive(150)", "from __main__ import factorial_tail_recursive")
 print ""
 print "factorial_tail_recursive()"
-print "timeit: " + str(timer2.timeit(repeats))
-print "custom: " + str(performance(factorial_tail_recursive, num))
+print "timeit:          " + str(timer2.timeit(repeats))
+print "custom:          " + str(performance(factorial_tail_recursive, num))
+factorial_tail_recursive_resource = resource_performance(factorial_tail_recursive, num)
+print "resource user:   " + str(factorial_tail_recursive_resource[0])
+print "resource system: " + str(factorial_tail_recursive_resource[1])
 
 timer3 = timeit.Timer("factorial_iter_w(150)", "from __main__ import factorial_iter_w")
 print ""
 print "factorial_iter_w()"
-print "timeit: " + str(timer3.timeit(repeats))
-print "custom: " + str(performance(factorial_iter_w, num))
+print "timeit:          " + str(timer3.timeit(repeats))
+print "custom:          " + str(performance(factorial_iter_w, num))
+factorial_iter_w_resource = resource_performance(factorial_iter_w, num)
+print "resource user:   " + str(factorial_iter_w_resource[0])
+print "resource system: " + str(factorial_iter_w_resource[1])
 
 timer4 = timeit.Timer("factorial_iter_f(150)", "from __main__ import factorial_iter_f")
 print ""
 print "factorial_iter_f()"
-print "timeit: " + str(timer4.timeit(repeats))
-print "custom: " + str(performance(factorial_iter_f, num))
+print "timeit:          " + str(timer4.timeit(repeats))
+print "custom:          " + str(performance(factorial_iter_f, num))
+factorial_iter_f_resource = resource_performance(factorial_iter_f, num)
+print "resource user:   " + str(factorial_iter_f_resource[0])
+print "resource system: " + str(factorial_iter_f_resource[1])
 
 print ""
 print "==========================================================================="
