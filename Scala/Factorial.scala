ee5e577e0420207e1f7bd307284be814f8215f10
JCPedroza <nemesis4go10@hotmail.com>
2013-10-06 17:30:27 -0600

    implemented both recursive and tail recursive factorial in Factorial.scala, removed FactorialTR.scala

--- Scala/Factorial.scala
+++ Scala/Factorial.scala
@@ -1,7 +1,13 @@
 object Factorial{
 
   /** Computes factorial of n. */
-  def factorial(n: Int): Int = 
-    if (n == 0) 1 else n * factorial(n - 1)
-
+  def factorialRecursive(n: Int): Int = 
+    if (n < 1) 1 else n * factorialRecursive(n - 1)
+  
+  /** Computes factorial of n using tail recursion */
+  def factorialTailRecursive(n: Int): Int = {
+    def recursion(n: Int, acc: Int): Int = 
+      if (n < 1) acc else recursion(n - 1, acc * n)
+    recursion(n, 1)
+  }  
 }
