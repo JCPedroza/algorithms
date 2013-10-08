b46e88e59e1ba262c962b29039d6a25156e03363
JCPedroza <nemesis4go10@hotmail.com>
2013-10-07 22:45:33 -0600

    important comments to the incl method

--- Scala/IntFunSetsBinaryT.scala
+++ Scala/IntFunSetsBinaryT.scala
@@ -62,7 +62,8 @@ class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet{
     if      (x < elem) left contains x
     else if (x > elem) right contains x
     else true
-    
+  
+  // Calls nodes until it reaches an empty node, where x is included.  
   def incl(x: Int): IntSet =
     if      (x < elem) new NonEmpty(elem, left incl x, right)
     else if (x > elem) new NonEmpty(elem, left, right incl x)
