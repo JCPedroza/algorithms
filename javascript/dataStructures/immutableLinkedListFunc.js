3c23bb667cbd85aa1d680b686dd6c7685f5e2709
JCPedroza <nemesis4go10@hotmail.com>
2013-10-17 13:09:35 -0600

    head now checks for Nil

--- javascript/dataStructures/immutableLinkedListFunc.js
+++ javascript/dataStructures/immutableLinkedListFunc.js
@@ -7,7 +7,7 @@
 * inserting a new value.
 *
 * The lists we construct using this method consist internally of nested arrays of 2 items. For example, the list [1, 2, 3] 
-* is represented by the expression cons(1, cons(2, cons(3, Nil))) which evaluates to the nested arrays [1, [2, [3, Nil]]].
+* is represented by the expression cons(1, cons(2, cons(3, Nil))) which evaluates to the nested arrays [1, [2, [3, []]]].
 *
 */
 
@@ -22,7 +22,7 @@ function cons(head, tail) {
 /**
 * Define list instances using a more convenient syntax and without deeply nested cons calls.
 * lst() === Nil                                                                                                                                                                
-* lst(1, 2, 3) === [1, [2, [3, Nil]]]
+* lst(1, 2, 3) === [1, [2, [3, []]]]
 */
 function lst() {
     var arg_array = Array.prototype.slice.call(arguments);  // arguments cast from object to array
@@ -32,7 +32,8 @@ function lst() {
 
 /** Returns the first element of a list */
 function head(xs) {
-    return xs[0];
+    if (xs === Nil) return Nil;
+    else return xs[0];
 }
 
 /** Returns a list containing all elements except the first. */
@@ -83,41 +84,29 @@ function take(n, xs){
 }
 
 /** Returns a copy of input list, without the first n elements. */
-function drop(n, xs){
+function drop(n, xs) {
     if (n < 1) return xs;
     else return drop(n - 1, tail(xs));
 }
 
 /**  Returns a subset of the input list that includes the items in the range from, to. */
-function piece(from, to, xs){
+function piece(from, to, xs) {
     return take(to - from, drop(from, xs));
 }
 
 /** Removes the element of the list at given index */
-function removeOne(index, xs){
+function removeOne(index, xs) {
     return concat(take(index - 1, xs), drop(index, xs));
 }
 
+// Some examples:
 
+var a = lst(1, 2, 3, 4, 5);
+var empty = lst();
 
-var a = lst();
-var zz = lst(1, 2, 3, 4, 5, 6, 7);
-var b = cons(7, cons(8, cons(9)));
-var g = cons(1, cons(2, cons(3, cons(4, cons(5, cons(6))))));
-console.log(JSON.stringify(g));
-console.log(JSON.stringify(a));
-console.log(length(g));
-console.log(length(a));
-console.log(JSON.stringify(concat(b, g)));
-console.log(last(g));
-console.log(JSON.stringify(init(g)));
-console.log(JSON.stringify(reverse(g)));
-console.log(JSON.stringify(zz));
-console.log(JSON.stringify(take(3, g)));
-console.log(JSON.stringify(drop(2, g)));
-console.log(JSON.stringify(piece(2, 4, g)));
-console.log(JSON.stringify(removeOne(2, g)));
-console.log(lst());
+console.log("empty: ", JSON.stringify(empty));
+console.log("head of empty: ", JSON.stringify(head(empty)));
+
 
 
 
