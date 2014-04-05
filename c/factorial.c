c6b6e6d64ca5339627e35db0e56cf436c86658b0
JCPedroza <nemesis4go10@hotmail.com>
2014-04-05 08:37:40 -0700

    timing function is now on a separate file

--- c/factorial.c
+++ c/factorial.c
@@ -8,21 +8,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "timeutils.h"
 
 // =========================================================================
 //                               Functions
 // =========================================================================
 
 // Using for loop
-double iterative_for_factorial(double n) {
-    double acc = 1;
+long iterative_for_factorial(long n) {
+    long acc = 1;
     for (n = n; n > 0; n--) acc *= n;
     return acc;
 }
 
 // Using while loop
-double iterative_while_factorial(double n) {
-    double acc = 1;
+long iterative_while_factorial(long n) {
+    long acc = 1;
     while (n > 0) {
         acc *= n;
         n--;
@@ -31,27 +32,14 @@ double iterative_while_factorial(double n) {
 }
 
 // Using recursion
-double recursive_factorial(double n) {
+long recursive_factorial(long n) {
     if (n < 1) return 1;
     return n * recursive_factorial(n-1);
 }
 
 // Using recursion and ternary operator
-double recursive_ternary_factorial(double n) {
+long recursive_ternary_factorial(long n) {
     return n < 1 ? 1 : n * recursive_ternary_factorial(n - 1);
-}
-
-// =========================================================================
-//                               Timing
-// =========================================================================
-
-// Measures the CPU time of a function executed x times
-double timeIt(double n, double times, double(*f)(double)) {
-    clock_t start = clock();
-    for (double i = 0; i < times; i++) {
-        f(n);
-    }
-    return (clock() - start);
 }
 
 // =========================================================================
@@ -65,13 +53,13 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    double number = atof(argv[1]);
-    double times  = atof(argv[2]);
+    long number = atol(argv[1]);
+    long times  = atol(argv[2]);
 
-    double iterative_for_time     = timeIt(number, times, iterative_for_factorial) / 1000000.0;
-    double iterative_while_time   = timeIt(number, times, iterative_while_factorial) / 1000000.0;
-    double recursive_time         = timeIt(number, times, recursive_factorial) / 1000000.0;
-    double recursive_ternary_time = timeIt(number, times, recursive_ternary_factorial) / 1000000.0;
+    double iterative_for_time     = time_it(number, times, iterative_for_factorial);
+    double iterative_while_time   = time_it(number, times, iterative_while_factorial);
+    double recursive_time         = time_it(number, times, recursive_factorial);
+    double recursive_ternary_time = time_it(number, times, recursive_ternary_factorial);
 
     double iterative_for_average     = iterative_for_time     / times;
     double iterative_while_average   = iterative_while_time   / times;
@@ -82,8 +70,8 @@ int main(int argc, char *argv[]) {
     printf("==============================================\n");
     printf("==============================================\n\n");
     printf("Factorial Algorithm\n");
-    printf("Factorial calculated: %f\n", number);
-    printf("Number of times: %f\n", times);
+    printf("Factorial calculated: %ld\n", number);
+    printf("Number of times: %ld\n", times);
     printf("(all results are in seconds) \n");
     printf("----------------------------------------------\n");
     printf("\n");
