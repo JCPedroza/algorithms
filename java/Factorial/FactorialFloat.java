3baeba28c7fb27280e3bd0187144099412bf8905
JCPedroza <nemesis4go10@hotmail.com>
2014-04-05 02:01:04 -0700

    some cleaning

--- java/Factorial/FactorialFloat.java
+++ java/Factorial/FactorialFloat.java
@@ -4,18 +4,9 @@ public class FactorialLong{
     /** Computes n factorial using recursion. */
     public float factorialRecursive(float n){
         if (n < 1) return 1;
-        else       return n * factorialRecursive(n - 1); 
+        return n * factorialRecursive(n - 1); 
     }
-    
-    /** Computes n factorial using tail recursion. */
-    public float factorialTailRecursive(float n){
-        return tailHelper(n, 1);
-    }
-    private float tailHelper(float n, float acc){
-        if (n < 1) return acc;
-        else       return tailHelper(n - 1, acc * n);
-    }
-    
+        
     /** Computes n factorial using iteration and while loop. */
     public float factorialIterW(float n){
         float acc = 1;
@@ -36,15 +27,7 @@ public class FactorialLong{
     
     
     public static void main(String[] args){
-        Factorial f = new Factorial();
-        
-        System.out.println(f.factorialRecursive(4));
-        System.out.println(f.factorialRecursive(5));
-        System.out.println(f.factorialTailRecursive(4));
-        System.out.println(f.factorialTailRecursive(5));
-        System.out.println(f.factorialIterF(4));
-        System.out.println(f.factorialIterF(5));
-        System.out.println(f.factorialIterW(4));
-        System.out.println(f.factorialIterW(5));
+    
+
     }
 }
