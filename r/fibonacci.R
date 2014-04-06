5286e887137f4a17ef73bf1efd31c1b438812560
JCPedroza <nemesis4go10@hotmail.com>
2014-04-05 21:01:58 -0700

    simplified recursive algorith

--- r/fibonacci.R
+++ r/fibonacci.R
@@ -5,8 +5,7 @@
 
 # Calculates the nth fibonacci number, recusrively, without tail recursion.
 # Performance: O(2^n).
-fiboRecursive <- function(n){
-  if      (n == 0) 0
-  else if (n == 1) 1
+fiboRecursive <- function(n) {
+  if (n < 2) n
   else fiboRecursive(n - 1) + fiboRecursive(n - 2)
 }
