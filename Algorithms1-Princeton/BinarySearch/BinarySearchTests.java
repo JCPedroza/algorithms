527d601ab836037c9840c36e519db5a807edcb3d
JCPedroza <nemesis4go10@hotmail.com>
2014-04-05 20:48:07 -0700

    deleted some prints

--- Algorithms1-Princeton/BinarySearch/BinarySearchTests.java
+++ Algorithms1-Princeton/BinarySearch/BinarySearchTests.java
@@ -6,12 +6,9 @@ public class BinarySearchTests{
         BinarySearch binarySearch = new BinarySearch();
 
         // Declare some arrays:
-        int[] array1 = new int[] {1, 2, 3, 4, 5, 6};
+        int[] array1 = new int[] {3, 6, 9, 12, 15, 18};
         int[] array2 = new int[] {2, 15, 17, 24, 35, 46, 57, 100, 1000, 2333};
     
-        System.out.println(binarySearch.binarySearch(array1, 4));  // 3
-        System.out.println(binarySearch.binarySearch(array1, 7));  // -1
-        System.out.println(binarySearch.binarySearch(array2, 35)); // 4
-        System.out.println(binarySearch.binarySearch(array2, 36)); // -1
+        System.out.println(binarySearch.binarySearch(array1, 6));  
     }
 }
