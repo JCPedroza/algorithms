cdab262d76dca7330724f242ac80dc48d893d490
JCPedroza <nemesis4go10@hotmail.com>
2014-09-28 17:24:33 -0600

    convolve

--- python/DSP/plot_here.py
+++ python/DSP/plot_here.py
@@ -1,5 +1,6 @@
 from wave_builders import *
 from plotutils import *
+import numpy as np
 
 sine1 = sine()
 tri1 = triangle()
@@ -7,5 +8,6 @@ tri2 = triangle2()
 saw1 = sawtooth()
 saw2 = sawtooth2()
 sqr1 = square()
+a_wave = np.convolve(sine1, sqr1)
 
-plot_all(sine1, tri1, tri2, saw1, saw2, sqr1)
+plot_all(a_wave)
