24a7e22278a5cea5cf6c755193fbc86a4e03ff85
JCPedroza <nemesis4go10@hotmail.com>
2015-02-19 08:05:36 -0600

    improved

--- python/2048/Merge/Merge1.py
+++ python/2048/Merge/Merge1.py
@@ -6,11 +6,11 @@ def slide(line):
     """
     Slides all numbers to the left.
     """
-    return_list = [0 for dummy_i in range(len(line))]
+    return_list = [0 for i in range(len(line))]
     available = 0
-    for dummy_i in line:
-        if dummy_i != 0:
-            return_list[available] = dummy_i
+    for i in line:
+        if i != 0:
+            return_list[available] = i
             available += 1
     return return_list
 
@@ -20,9 +20,9 @@ def merge(line):
     """
     slide_list = slide(line)
     
-    for dummy_i in range(len(slide_list) - 1):
-        if slide_list[dummy_i] == slide_list[dummy_i + 1]:
-            slide_list[dummy_i] *= 2
-            slide_list[dummy_i + 1] = 0
+    for i in range(len(slide_list) - 1):
+        if slide_list[i] == slide_list[i + 1]:
+            slide_list[i] *= 2
+            slide_list[i + 1] = 0
             
     return slide(slide_list)
--- python/2048/Merge/Merge2.py
+++ python/2048/Merge/Merge2.py
@@ -6,9 +6,9 @@ def first_not_zero(line):
     """
     Returns the first non-zero value in a list.
     """
-    for dummy_i in line:
-        if dummy_i != 0:
-            return dummy_i
+    for i in line:
+        if i != 0:
+            return i
         
     return False  # no non-zero values found
 
@@ -22,25 +22,25 @@ def merge(line):
     jump = False
     zeros = 0
     
-    for dummy_i in line:
-        if dummy_i == 0:
+    for i in line:
+        if i == 0:
             index += 1
             zeros += 1
         else:
             if jump == False:
                 nextval = first_not_zero(line[index+1:])
-                if dummy_i == nextval:
-                    return_list.append(dummy_i * 2)
+                if i == nextval:
+                    return_list.append(i * 2)
                     zeros += 1
                     jump = True
                 else:
-                    return_list.append(dummy_i)
+                    return_list.append(i)
                 index += 1
             else:
                 index += 1
                 jump = False
                 
-    for dummy_i in range(zeros):
+    for i in range(zeros):
         return_list.append(0)
             
     return return_list
