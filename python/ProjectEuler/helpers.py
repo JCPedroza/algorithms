3bcd4256e697b68ed6cec16eb0a90be0e7edcd7a
JCPedroza <nemesis4go10@hotmail.com>
2015-07-24 17:24:09 -0500

    linted

--- python/ProjectEuler/helpers.py
+++ python/ProjectEuler/helpers.py
@@ -4,6 +4,7 @@ Helper functions.
 
 import math
 
+
 def is_prime(n):
     """
     Primality test.
@@ -11,7 +12,7 @@ def is_prime(n):
     algorithm (deterministic in that range), else use trial division.
     """
 
-    def mr(n, _known_primes = [2, 3], _precision_for_huge_n=16, ):
+    def mr(n, _known_primes=[2, 3], _precision_for_huge_n=16, ):
 
         def _try_composite(a, d, n, s):
             if pow(a, d, n) == 1:
@@ -19,7 +20,7 @@ def is_prime(n):
             for i in range(s):
                 if pow(a, 2**i * d, n) == n-1:
                     return False
-            return True # n  is definitely composite
+            return True  # n  is definitely composite
 
         if n in _known_primes:
             return True
@@ -32,22 +33,22 @@ def is_prime(n):
             d, s = d >> 1, s + 1
 
         # Returns exact according to http://primes.utm.edu/prove/prove2_3.html
-        if n < 1373653: 
+        if n < 1373653:
             return not any(_try_composite(a, d, n, s) for a in (2, 3))
-        if n < 25326001: 
+        if n < 25326001:
             return not any(_try_composite(a, d, n, s) for a in (2, 3, 5))
-        if n < 118670087467: 
-            if n == 3215031751: 
+        if n < 118670087467:
+            if n == 3215031751:
                 return False
             return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7))
-        if n < 2152302898747: 
+        if n < 2152302898747:
             return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
-        if n < 3474749660383: 
+        if n < 3474749660383:
             return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
-        if n < 341550071728321: 
+        if n < 341550071728321:
             return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
         # otherwise
-        return not any(_try_composite(a, d, n, s) 
+        return not any(_try_composite(a, d, n, s)
                        for a in _known_primes[:_precision_for_huge_n])
 
     def trial_division(n):
@@ -58,7 +59,7 @@ def is_prime(n):
         if n % 2 == 0 or n % 3 == 0:
             return False
 
-        limit   = int(math.sqrt(n))
+        limit = int(math.sqrt(n))
         divisor = 5
 
         while divisor <= limit:
@@ -68,7 +69,7 @@ def is_prime(n):
 
         return True
 
-    if 30000000  < n < 341550071728321:
+    if 30000000 < n < 341550071728321:
         return mr(n)
     else:
         return trial_division(n)
